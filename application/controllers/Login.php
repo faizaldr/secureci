715c63fa96cf1cc614f384174954250a345a1315
faizaldr <fdr00zenc@gmail.com>
2023-09-20 10:18:12 +0700

    hardening

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,10 +1,36 @@
 <?php
-class Login extends ci_controller{
-    function __construct(){
+class Login extends ci_controller
+{
+    function __construct()
+    {
         parent::__construct();
     }
-    function index(){
+    function index()
+    {
         $this->load->view("login/vLogin");
     }
 
+    function do_login()
+    {
+        $username = $this->input->post("username");
+        $password = md5($this->input->post("password"));
+        // $query = $this->db->query("select * from user where username='$username' and password='$password' ");
+        $query = $this->db->query("select * from user where username=? and password=? ",[$username,$password]);
+        if ($query->num_rows() == 1) {
+            $row = $query->row();
+            $this->session->set_userdata(["user_id" => $row->user_id, "login" => true, "username" => $username, "nama" => $row->nama, "level" => $row->user_level_id]);
+            $this->db->update(
+                "user",
+                [
+                    'user_last_login' => date('Y-m-d H:i:s'),
+                    'user_last_ip' => $this->input->ip_address(),
+                ],
+                ['username' => $username]
+            );
+            redirect("dashboard");
+        } else {
+            $this->session->set_flashdata("pesan_error", "Gagal Login Username/Password Salah!");
+            redirect("login");
+        }
+    }
 }
